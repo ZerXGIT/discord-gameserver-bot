c432d630e607d3a0540b864a89c560f04755bd08
ZerXGIT <65255933+ZerXGIT@users.noreply.github.com>
2021-09-19 14:30:15 +0200

    Initial commit

---
+++ src/main/java/de/zerx/Main.java
@@ -0,0 +1,23 @@
+package de.zerx;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * Created by Till O. aka. ZerX
+ * <p>
+ * Project name: Discord-GameServer-Bot<p>
+ * This file is created at 19.09.2021 13:53
+ */
+public class Main {
+
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault("").build();
+
+    }
+
+
+
+}
