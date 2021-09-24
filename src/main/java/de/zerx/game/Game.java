8dfc805327b80041480bbbd3c43102533cc9cdee
Till O <blackminer1242@gmail.com>
2021-09-24 10:29:12 +0200

    Update2

---
+++ src/main/java/de/zerx/game/Game.java
@@ -0,0 +1,22 @@
+package de.zerx.game;
+
+/**
+ * Created by Till O. aka. ZerX
+ * <p>
+ * Project name: Discord-GameServer-Bot<p>
+ * This file was created at 23.09.2021 23:20
+ */
+public enum Game {
+
+    GarrysMod("Garry's Mod"),
+    Minecraft("Minecraft"),
+    CsGo("CS:GO");
+
+    String Name;
+
+    Game(String Name) {
+        this.Name = Name;
+    }
+
+
+}
